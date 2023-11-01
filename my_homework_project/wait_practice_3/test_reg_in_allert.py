c54deee4799c350985bf69db449af2dd41c46bce
Aleksei Tumakov <109537858+gomboc665@users.noreply.github.com>
2023-11-01 20:24:01 +0400

    Update test_reg_in_allert.py
    update

--- my_homework_project/wait_practice_3/test_reg_in_allert.py
+++ my_homework_project/wait_practice_3/test_reg_in_allert.py
@@ -5,11 +5,10 @@ def test_add(driver,wait):
     # Инициализация драйвера
     username = 'admin'
     password = 'admin'
-    url = f'https://{username}:{password}@the-internet.herokuapp.com/ basic_auth'
+    url = f'https://{username}:{password}@the-internet.herokuapp.com/basic_auth'
     
     # Вводим логин и пароль с помощью pyautogui
     driver.get(url)
 
     assert wait.until(EC.text_to_be_present_in_element((By.XPATH,'//p'),'Congratulations! You must have the proper credentials.'))
-
     

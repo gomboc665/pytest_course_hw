76c2d49ccc814e2ca7a2e35a9191f6511ea32d07
Aleksei Tumakov <109537858+gomboc665@users.noreply.github.com>
2023-11-12 01:24:44 +0400

    update chrome options for CI workflow

--- my_homework_project/wait_practice_3/conftest.py
+++ my_homework_project/wait_practice_3/conftest.py
@@ -5,12 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
-
 @pytest.fixture
-def chrome_options():
+def options():
     options = Options()
-    options.add_argument("--start-maximized")
-    # options.add_argument('--headless')
+    options.add_argument('--window-size=2880,1800')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     return options
 
 

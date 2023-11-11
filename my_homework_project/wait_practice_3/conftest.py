de914f41499ef8b7d16e673f9c92b4a213436385
Aleksei Tumakov <109537858+gomboc665@users.noreply.github.com>
2023-11-12 02:10:56 +0400

    Update conftest.py

--- my_homework_project/wait_practice_3/conftest.py
+++ my_homework_project/wait_practice_3/conftest.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
+
 @pytest.fixture
 def chrome_options():
     options = Options()
